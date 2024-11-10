09adbe885d0157d751eb3d4ec1cba75ea10fb3ff
Md Refat Hossain <refatibnanwar@gmail.com>
2024-11-10 12:46:48 +0800

    Initial Commit

---
+++ RHKeyboardProject/RHKeyboardProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RHKeyboardProjectApp.swift
+//  RHKeyboardProject
+//
+//  Created by Md Refat Hossain on 10/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RHKeyboardProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
